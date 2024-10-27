5c4f438664b10c99469e4888c71c78e6203c21a2
Krish Mittal <krishmittal212005@gmail.com>
2024-10-27 16:21:42 +0530

    Initial Commit

---
+++ pannelanimation-dribbleex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pannelanimation-dribbleex
+//
+//  Created by Krish Mittal on 27/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pannelanimation-dribbleex/pannelanimation_dribbleexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pannelanimation_dribbleexApp.swift
+//  pannelanimation-dribbleex
+//
+//  Created by Krish Mittal on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct pannelanimation_dribbleexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
